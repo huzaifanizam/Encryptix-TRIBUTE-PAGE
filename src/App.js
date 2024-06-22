09493015e9a332c5e27f4b62dff36cffc45b0c02
HUZAIFA NIZAM <121801411+huzaifanizam@users.noreply.github.com>
2024-06-23 03:33:33 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Porfile from './pages/Porfile';
+
+function App() {
+  return (
+    <div className="App">
+      <Porfile />
+    </div>
+  );
+}
+
+export default App;
